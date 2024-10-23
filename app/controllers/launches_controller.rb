068602b15265a506c7732786f16390207a20859f
Adrian Grimm <usmcamgrimm@gmail.com>
2024-10-23 09:45:38 -0400

    Trying a query

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,4 +1,22 @@
 class LaunchesController < ApplicationController
   def index
+    response = HTTParty.get('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/')
+    @launchData = parse_data(response)
+  end
+
+  private
+
+  def parse_data(response)
+    response['results'].map do |launch|
+      {
+        name: launch['name'],
+        mission: launch['mission'],
+        deescription: launch['description'],
+        rocketname: launch['rocketname'],
+        launch_date: launch['launch_date'],
+        status: launch['status'],
+        location: launch['location']
+      }
+    end
   end
 end
