477174db65729636cdad2f1b8888c4171ab39222
Adrian Grimm <usmcamgrimm@gmail.com>
2024-11-14 09:49:59 -0500

    Format time and date

--- app/helpers/launches_helper.rb
+++ app/helpers/launches_helper.rb
@@ -1,2 +1,11 @@
 module LaunchesHelper
+  def set_launch_date(launch_date)
+    date = Date.parse(launch_date)
+    date.strftime("%A, %d/%m/%Y")
+  end
+
+  def set_launch_time(launch_date)
+    time = Time.parse(launch_date)
+    time.strftime("%I:%m%p")
+  end
 end
