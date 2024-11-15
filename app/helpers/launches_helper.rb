fccad526bf8ed1e097ffae100bf5da8cae1086d3
Adrian Grimm <usmcamgrimm@gmail.com>
2024-11-15 09:22:54 -0500

    Update time and date format

--- app/helpers/launches_helper.rb
+++ app/helpers/launches_helper.rb
@@ -1,11 +1,11 @@
 module LaunchesHelper
   def set_launch_date(launch_date)
     date = Date.parse(launch_date)
-    date.strftime("%A, %d/%m/%Y")
+    date.strftime("%A, %b %-d")
   end
 
   def set_launch_time(launch_date)
     time = Time.parse(launch_date)
-    time.strftime("%I:%m%p")
+    time.strftime("%I:%m%P")
   end
 end
