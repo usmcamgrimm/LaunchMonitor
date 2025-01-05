65a89dc7deb0d7cef61f39187eb0deb81d2f9993
Adrian Grimm <usmcamgrimm@gmail.com>
2025-01-05 12:54:55 -0500

    Set the production API in the controller

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,7 +1,7 @@
 class LaunchesController < ApplicationController
   def index
-    response = HTTParty.get("https://lldev.thespacedevs.com/2.2.0/launch/upcoming/")
-    # response = HTTParty.get("https://ll.thespacedevs.com/2.3.0/launches/upcoming/")
+    # response = HTTParty.get("https://lldev.thespacedevs.com/2.2.0/launch/upcoming/")
+    response = HTTParty.get("https://ll.thespacedevs.com/2.3.0/launches/upcoming/")
     @launchData = parse_data(response)
     @locations = @launchData.map { |launch| launch[:location] }.uniq
 
@@ -15,8 +15,8 @@ class LaunchesController < ApplicationController
   def parse_data(response)
     response["results"].map do |launch|
       {
-        image: launch["image"],
-        # image: launch.dig("image", "image_url") || "stock_launch.jpg",
+        # image: launch["image"],
+        image: launch.dig("image", "image_url") || "stock_launch.jpg",
         lsp: launch.dig("launch_service_provider", "name") || "No agency provided",
         name: launch["name"],
         payload: launch.dig("mission", "name") || "Unidentified payload",
