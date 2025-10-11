59e60a118fedf13a1dc4e9c459427b4862892146
Adrian Grimm <usmcamgrimm@gmail.com>
2025-10-11 15:34:59 -0400

    Why does this only work with the dev API????
    Trying to save location preferences

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,22 +1,37 @@
 class LaunchesController < ApplicationController
   def index
-    # response = HTTParty.get("https://lldev.thespacedevs.com/2.2.0/launch/upcoming/")
-    response = HTTParty.get("https://ll.thespacedevs.com/2.3.0/launches/upcoming/")
+    response = HTTParty.get("https://lldev.thespacedevs.com/2.2.0/launch/upcoming/")
+    # response = HTTParty.get("https://ll.thespacedevs.com/2.3.0/launches/upcoming/")
     @launchData = parse_data(response)
     @locations = @launchData.map { |launch| launch[:location] }.uniq
 
-    if params[:locations].present?
-      @launchData = @launchData.select { |launch| params[:locations].include?(launch[:location]) }
+    # --- Handle Preferences ---
+    case params[:commit]
+    when "apply"
+      if params[:locations].present?
+        session[:selected_locations] = params[:locations]
+      end
+    when "reset"
+      session.delete(:selected_locations)
+    end
+
+    selected_locations = session[:selected_locations]
+
+    # Apply saved preferences if any
+    if selected_locations.present?
+      @launchData = @launchData.select { |launch| selected_locations.include?(launch[:location]) }
     end
   end
 
   private
 
   def parse_data(response)
-    response["results"].map do |launch|
+    results = response.parsed_response["results"] rescue []
+    return [] if results.nil?
+
+    results.map do |launch|
       {
-        # image: launch["image"],
-        image: launch.dig("image", "image_url") || "stock_launch.jpg",
+        image: launch["image"] || "stock_launch.jpg",
         lsp: launch.dig("launch_service_provider", "name") || "No agency provided",
         name: launch["name"],
         payload: launch.dig("mission", "name") || "Unidentified payload",
--- app/javascript/controllers/location_filter_controller.js
+++ app/javascript/controllers/location_filter_controller.js
@@ -1,13 +1,10 @@
-import {Controller }  from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["checkbox"]
 
   reset(event) {
     event.preventDefault()
-    console.log("Reset clicked!")
-    this.checkboxTargets.forEach(checkbox => {
-      checkbox.checked = false
-    })
+    this.checkboxTargets.forEach(cb => cb.checked = false)
   }
 }
