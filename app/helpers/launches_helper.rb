e99f41b14aa13aab347b77276eac5916b153af5c
Adrian Grimm <usmcamgrimm@gmail.com>
2026-04-27 13:23:07 -0400

    Add a fallback color to the status case helper

--- app/helpers/launches_helper.rb
+++ app/helpers/launches_helper.rb
@@ -14,6 +14,7 @@ module LaunchesHelper
     when "Go for Launch", "Launch Successful" then "text-green-500 hover:text-green-600"
     when "Launch in Flight", "Payload Deployed" then "text-sky-500 hover:text-sky-600"
     when "To Be Confirmed", "To Be Determined" then "text-orange-500 hover:text-orange-600"
+    else "text-slate-200 hover:text-slate-400"
     end
   end
 end
