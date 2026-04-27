5cf173e81d4d2cb63b497027000bbea405c1aa22
Adrian Grimm <usmcamgrimm@gmail.com>
2026-04-27 10:04:03 -0400

    Fix accessibility issues
    Added alt text for images based on agency and rocket type
    Moved the status color case statement to a helper method
    Added aria labels to the rocket/mission name and the rocket icon

--- app/helpers/launches_helper.rb
+++ app/helpers/launches_helper.rb
@@ -8,4 +8,12 @@ module LaunchesHelper
     time = Time.parse(launch_date).in_time_zone(Time.zone)
     time.strftime("%l:%M%P")
   end
+
+  def status_color(status)
+    case status
+    when "Go for Launch", "Launch Successful" then "text-green-500 hover:text-green-600"
+    when "Launch in Flight", "Payload Deployed" then "text-sky-500 hover:text-sky-600"
+    when "To Be Confirmed", "To Be Determined" then "text-orange-500 hover:text-orange-600"
+    end
+  end
 end
