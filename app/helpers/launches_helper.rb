359c33c3d6ba91aee233e002dabec4303a3f5b1b
Adrian Grimm <usmcamgrimm@gmail.com>
2026-05-09 09:51:07 -0400

    Show launch times in the User's local timezone
    Removed the timezone cookie and the call in the view template
    Removed the now-unused helper method
    Added Stimulus controller to format the time zone in the view template

--- app/helpers/launches_helper.rb
+++ app/helpers/launches_helper.rb
@@ -1,11 +1,2 @@
 module LaunchesHelper
-  def set_launch_date(launch_date)
-    date = Date.parse(launch_date)
-    date.strftime("%A, %b %-d")
-  end
-
-  def set_launch_time(launch_date)
-    time = Time.parse(launch_date).in_time_zone(Time.zone)
-    time.strftime("%l:%M%P")
-  end
 end
