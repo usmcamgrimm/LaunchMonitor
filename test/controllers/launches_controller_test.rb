3bc05692748d8068e312b35fb328b2a2df2d00ef
Adrian Grimm <usmcamgrimm@gmail.com>
2026-05-21 06:23:35 -0400

    Revert "Merge pull request #143 from usmcamgrimm/api/cache"
    This reverts commit de2f8172f09ec4e1dc49d7542c0ec6922e3eb59a, reversing
    changes made to acb937235c8360e5479b7258adfd43f840c33faa.

--- test/controllers/launches_controller_test.rb
+++ test/controllers/launches_controller_test.rb
@@ -1,50 +1,7 @@
 require "test_helper"
-require "webmock/minitest"
 
 class LaunchesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    Rails.cache.clear
-  end
-
-  teardown do
-    Rails.cache.clear
-  end
-
-  test "shows friendly upstream error when refresh fails without cached launches" do
-    stub_request(:get, %r{thespacedevs\.com}).to_timeout
-    RefreshLaunchesJob.perform_now
-
-    get root_url
-
-    assert_response :success
-    assert_match(/Unable to reach TheSpaceDevs API/i, response.body)
-    assert_no_match(/No launches found/i, response.body)
-  end
-
-  test "keeps showing cached launches when a later refresh fails" do
-    stub_request(:get, %r{thespacedevs\.com})
-      .to_return(
-        status: 200,
-        body: {
-          results: [
-            {
-              name: "Starlink Group 42",
-              net: "2026-05-20T10:00:00Z",
-              launch_service_provider: { name: "SpaceX" }
-            }
-          ]
-        }.to_json,
-        headers: { "Content-Type" => "application/json" }
-      ).then
-      .to_timeout
-
-    RefreshLaunchesJob.perform_now
-    RefreshLaunchesJob.perform_now
-
-    get root_url
-
-    assert_response :success
-    assert_match(/Starlink Group 42/i, response.body)
-    assert_no_match(/Unable to reach TheSpaceDevs API/i, response.body)
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
